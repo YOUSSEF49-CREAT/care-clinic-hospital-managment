82bb87925eb206193951af384d5fae576b973b4e
youssef faouzi <yj1984515@gmail.com>
2025-12-17 10:41:12 +0100

    Fix undefined dashboard statistics variables

---
+++ includes/sidebar.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+  <a href="/care-clinic-hospital-managment/index.php">Dashboard</a>
+  <a href="/care-clinic-hospital-managment/departements/index.php">Départements</a>
+  <a href="/care-clinic-hospital-managment/medecins/index.php">Médecins</a>
+  <a href="/care-clinic-hospital-managment/patients/index.php">Patients</a>
+</div>
--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <?php
+include("config/db.php");
+
+$patientsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM patients"));
+$medecinsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM medecins"));
+$departementsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM departements"));
+
+
+?>
+
+<link rel="stylesheet" href="assets/style.css">
+<?php include("includes/sidebar.php"); ?>
+
+<div class="content">
+  <h2>Dashboard</h2>
+
+  <div class="dashboard">
+
+    <div class="card patients">
+      <h3>Patients</h3>
+      <p><?= $patientsCount ?></p>
+    </div>
+
+    <div class="card medecins">
+      <h3>Médecins</h3>
+      <p><?= $medecinsCount ?></p>
+    </div>
+
+    <div class="card departements">
+      <h3>Départements</h3>
+      <p><?= $departementsCount ?></p>
+    </div>
+
+
+   
+
+  </div>
+</div>
+
