69ed61a75d7a69b5929521b7ae109b6043d3dd0e
youssef faouzi <yj1984515@gmail.com>
2025-12-17 16:40:58 +0100

    Add charts and statistics to dashboard

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,6 @@
-<div class="sidebar">
+<div class="sidebar" style="width:15%;height:100%">
   <a href="/care-clinic-hospital-managment/index.php">Dashboard</a>
   <a href="/care-clinic-hospital-managment/departements/index.php">Départements</a>
   <a href="/care-clinic-hospital-managment/medecins/index.php">Médecins</a>
-  <a href="/care-clinic-hospital-managment/patients/index.php">Patients</a>
+  <a href="/care-clinic-hospital-managment/patients">Patients</a>
 </div>
--- index.php
+++ index.php
@@ -4,36 +4,64 @@ include("config/db.php");
 $patientsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM patients"));
 $medecinsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM medecins"));
 $departementsCount = mysqli_num_rows(mysqli_query($conn," SELECT * FROM departements"));
-
-
 ?>
 
 <link rel="stylesheet" href="assets/style.css">
-<?php include("includes/sidebar.php"); ?>
 
-<div class="content">
-  <h2>Dashboard</h2>
+<body>
 
-  <div class="dashboard">
+    <?php include("includes/sidebar.php"); ?>
 
-    <div class="card patients">
-      <h3>Patients</h3>
-      <p><?= $patientsCount ?></p>
-    </div>
+    <section >
+        <div class="content">
+            <h2>Dashboard</h2>
 
-    <div class="card medecins">
-      <h3>Médecins</h3>
-      <p><?= $medecinsCount ?></p>
-    </div>
+            <div class="dashboard">
+                <div class="card patients">
+                    <h3>Patients</h3>
+                    <p><?= $patientsCount ?></p>
+                </div>
 
-    <div class="card departements">
-      <h3>Départements</h3>
-      <p><?= $departementsCount ?></p>
-    </div>
+                <div class="card medecins">
+                    <h3>Médecins</h3>
+                    <p><?= $medecinsCount ?></p>
+                </div>
 
+                <div class="card departements">
+                    <h3>Départements</h3>
+                    <p><?= $departementsCount ?></p>
+                </div>
+            </div>
 
-   
+            <div class="chart-container">
+                <canvas id="myChart"></canvas>
+            </div>
+        </div>
+    </section>
+</body>
 
-  </div>
-</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Patients', 'Médecins', 'Départements'],
+      datasets: [{
+        label: 'Count',
+        data: [<?= $patientsCount ?>, <?= $medecinsCount ?>, <?= $departementsCount ?>],
+        borderWidth: 1,
+        backgroundColor: ['#1abc9c', '#3498db', '#e67e22']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
